c2fe896582708627b39a0736009e181772a41ec0
Simon Gilbert <sah.gilbert@gmail.com>
2019-02-21 12:27:04 +0000

    Implement extension method and equivalent unit tests

---
+++ SimonGilbert.Blog.Extensions/DateTimeExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SimonGilbert.Blog.Extensions
+{
+    public static class DateTimeExtensions
+    {
+        public static DateTime RoundToNearestHour(this DateTime dateTime)
+        {
+            long ticks = dateTime.Ticks + 18000000000;
+
+            return new DateTime(ticks - ticks % 36000000000, dateTime.Kind);
+        }
+    }
+}
